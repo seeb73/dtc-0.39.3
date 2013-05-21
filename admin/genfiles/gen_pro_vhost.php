3a2f4c82259e986aac4ed6b91088b5d6c321a72d
Thomas Goirand <thomas@goirand.fr>
2013-05-21 20:32:51 +0800

    Also allow the "Header" directive.

--- admin/genfiles/gen_pro_vhost.php
+++ admin/genfiles/gen_pro_vhost.php
@@ -1071,7 +1071,7 @@ $vhost_file .= "
 	ErrorDocument 501 /sbox404/406.php
 	<Directory $web_path/$domain_to_get/subdomains.aufs/$web_subname/html>
 		AllowOverride AuthConfig Indexes Limit
-		AllowOverrideList ErrorDocument LanguagePriority AddCharset AddEncoding AddLanguage RemoveCharset RemoveEncoding SetEnvIf SetEnvIfNoCase BrowserMatch RewriteEngine RewriteOptions RewriteBase RewriteCond RewriteRule Redirect RedirectTemp RedirectPermanent RedirectMatch
+		AllowOverrideList ErrorDocument LanguagePriority AddCharset AddEncoding AddLanguage RemoveCharset RemoveEncoding SetEnvIf SetEnvIfNoCase BrowserMatch RewriteEngine RewriteOptions RewriteBase RewriteCond RewriteRule Redirect RedirectTemp RedirectPermanent RedirectMatch Header
 	</Directory>
 	Options +ExecCGI\n";
 							}
