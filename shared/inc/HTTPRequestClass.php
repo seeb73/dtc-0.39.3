5a0b96e1d80c12eccf4417922c0c36d0196e9c92
Damien Mascord <Damien.Mascord@sita.aero>
2016-12-12 13:39:58 +1100

    Update to use curl

--- shared/inc/HTTPRequestClass.php
+++ shared/inc/HTTPRequestClass.php
@@ -12,46 +12,46 @@ class dtc_HTTPRequest
     var $_protocol;    // protocol (HTTP/HTTPS)
     var $_uri;        // request URI
     var $_port;        // port
-    
-    // Timeout in seconds 
+
+    // Timeout in seconds
     var $_timeout = 5;
-    
+
     // scan url
     function _scan_url()
     {
         $req = $this->_url;
-        
+
         $pos = strpos($req, '://');
         $this->_protocol = strtolower(substr($req, 0, $pos));
-        
+
         $req = substr($req, $pos+3);
         $pos = strpos($req, '/');
         if($pos === false)
             $pos = strlen($req);
         $host = substr($req, 0, $pos);
-        
+
         if(strpos($host, ':') !== false)
         {
             list($this->_host, $this->_port) = explode(':', $host);
         }
-        else 
+        else
         {
             $this->_host = $host;
             $this->_port = ($this->_protocol == 'https') ? 443 : 80;
         }
-        
+
         $this->_uri = substr($req, $pos);
         if($this->_uri == '')
             $this->_uri = '/';
     }
-    
+
     // constructor
     function dtc_HTTPRequest($url)
     {
         $this->_url = $url;
         $this->_scan_url();
     }
-    
+
     // download URL to string Array
     function DownloadToStringArray()
     {
@@ -60,90 +60,52 @@ class dtc_HTTPRequest
     	$array = preg_split($crlf, $fullresponse, -1, PREG_SPLIT_NO_EMPTY);
 	return $array;
     }
-    
+
     // download URL to string
     function DownloadToString()
     {
     	// store errors in case we need to handle them
         $errno;
         $errstr;
-	$response ='';
-        
+		$response ='';
+
         $crlf = "\r\n";
-        
-        // generate request
-        $req = 'GET ' . $this->_uri . ' HTTP/1.0' . $crlf 
-            .    'Host: ' . $this->_host . $crlf 
-            .    $crlf;
-        
-        
-        // fetch from URL
-        $this->_fp = fsockopen(($this->_protocol == 'https' ? 'sslv3://' : '') . $this->_host, $this->_port, $errno, $errstr, $this->_timeout);
-        fwrite($this->_fp, $req);
-        
-        $a_vers = explode(".",phpversion());
-				$use_stream = TRUE;
-				if($a_vers[0] <= 4 && $a_vers[1] < 3)
-				{
-					// first < 4.3, then we need to use socket rather than stream
-					$use_stream = FALSE;
-				}
-				
-				if ($use_stream)
-				{
-					stream_set_blocking($this->_fp, TRUE); 
-	        stream_set_timeout($this->_fp,$this->_timeout); 
-				} 
-				else 
-				{
-	        socket_set_blocking($this->_fp, TRUE); 
-	        socket_set_timeout($this->_fp,$this->_timeout); 
-        }
-        
-        // get the socket status
-        $info;
-        if ($use_stream)
-        {
-        	$info = stream_get_meta_data($this->_fp);
-        } else {
-        	$info = socket_get_status($this->_fp);
-        }
-       
-        while(is_resource($this->_fp) && $this->_fp && !feof($this->_fp) && (!$info['timed_out']))
-        {
-            $response .= fread($this->_fp, 4096);
-            if ($use_stream)
-            {
-            	$info = stream_get_meta_data($this->_fp);
-            } else {
-            	$info = socket_get_status($this->_fp);
-            }
-            @ob_flush(); 
-            flush();
-        }
-        fclose($this->_fp);
-        
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->_protocol . "://" . $this->_host . $this->_uri);
+		//$fp = fopen('/tmp/curl_errorlog.txt', 'w');
+		//curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		//curl_setopt($ch, CURLOPT_STDERR, $fp);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: close'));
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->_timeout);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
         // split header and body
         $pos = strpos($response, $crlf . $crlf);
         if($pos === false)
             return($response);
         $header = substr($response, 0, $pos);
         $body = substr($response, $pos + 2 * strlen($crlf));
-        
+
         // parse headers
         $headers = array();
         $lines = explode($crlf, $header);
         foreach($lines as $line)
             if(($pos = strpos($line, ':')) !== false)
                 $headers[strtolower(trim(substr($line, 0, $pos)))] = trim(substr($line, $pos+1));
-        
+
         // redirection?
         if(isset($headers['location']))
         {
             $http = new dtc_HTTPRequest($headers['location']);
             return($http->DownloadToString($http));
         }
-        else 
+        else
         {
             return($body);
         }
